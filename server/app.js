654bafae4429ea680de900f2a888b5a10f59543a
Aladdin Sonni <aladdin.sonni@gmail.com>
2018-01-11 20:38:10 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1/', index);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('../client/build'));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+module.exports = app;
---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.json([{
+    id: 1,
+    username: "samsepi0l"
+  }, {
+    id: 2,
+    username: "D0loresH4ze"
+  }]);
+});
+
+module.exports = router;
